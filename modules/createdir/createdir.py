e8f71d4b087b0f96f7e48d1403f143e80ee4644a
Nacho Brihuega <brihuega.nacho@gmail.com>
2017-11-24 10:24:32 +0100

    Add files via upload

---
+++ modules/createdir/createdir.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os		
+
+def CreateDir(target):
+	try:
+
+		path_initial = None
+		path = None
+
+		if not os.path.exists(target):
+			os.mkdir(target)
+			temp = str(target) + '/temp'
+			print temp
+			os.mkdir(temp)
+
+	except Exception as e:
+		print e
